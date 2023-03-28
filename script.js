e62ea35c13a594115f19ecd5dbbe71994ef75e90
Olawoyin Abdulmuiz <abdulmuizolawoyin@gmail.com>
2023-03-28 11:59:22 +0100

    Create variables

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 console.log("Hello World!")
+let computerInput = ["rock", "paper", "scissors"]
+let paperWinRock = "Paper beats Rock" 
+let scissorsWinPaper = "Scissors beats paper"
+let rockWinScisssors = "Rock beats Scissors"
 function getComputerChoice() {
     
 }
