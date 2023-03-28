6e987643ea5a73e1594de53e731b25fdf81a7f15
Olawoyin Abdulmuiz <abdulmuizolawoyin@gmail.com>
2023-03-28 12:46:52 +0100

    Add if, else if and else statements

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let computerInput = ["rock", "paper", "scissors"]
 let paperWinRock = "Paper beats Rock" 
 let scissorsWinPaper = "Scissors beats paper"
 let rockWinScissors = "Rock beats Scissors"
-let draw = "It is a draw"
+let draw = "It is a draw!"
 
 function getComputerChoice() {
    computerInput[Math.floor(Math.random()*computerInput.length)]
@@ -11,8 +11,27 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection===computerSelection) {
-        console.log("")
+    if (playerSelection==="scissors"&&computerSelection==="rock") {
+        console.log(`You lose! ${rockWinScissors}`)
+    }
+    else if (playerSelection==="scissors"&&computerSelection==="paper") {
+        console.log(`You win! ${scissorsWinPaper}`)
+    }
+    
+    else if (playerSelection==="rock"&&computerSelection==="paper") {
+        console.log(`You lose! ${paperWinRock}`)
+    }
+    else if (playerSelection==="rock"&&computerSelection==="scissors") {
+        console.log(`You win! ${rockWinScissors}`)
+    }
+    else if (playerSelection==="paper"&&computerSelection==="rock") {
+        console.log(`You win! ${paperWinRock}`)
+    }
+    else if (playerSelection==="paper"&&computerSelection==="scissors") {
+        console.log(`You lose! ${scissorsWinPaper}`)
+    }
+    else {
+        console.log(draw)
     }
 
 }
