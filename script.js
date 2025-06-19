db54809c222ac7622f086ed232045ee6e43931b3
0la-lekan <abdulmuizolawoyin@gmail.com>
2025-06-19 10:37:47 +0100

    Play a round of game Change computerChoice to computerValue to avoid redundancy Create conditional statement to output appropriate statements according to inputs

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ console.log("Hello World");
 // step 2 --- generate computer choice
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random()*100);
-    let computerChoice;
+    let computerValue;
     // console.log(randomNum);
 
     if (randomNum >= 0 && randomNum < 31) {
-         computerChoice = "rock";
-         return computerChoice;
+         computerValue = "rock";
+         return computerValue;
     }
     else if (randomNum >= 31 && randomNum < 61) {
-        computerChoice = "paper";
-        return computerChoice;
+        computerValue = "paper";
+        return computerValue;
     }
     else {
-        computerChoice = "scissors";
-        return computerChoice;
+        computerValue = "scissors";
+        return computerValue;
     }
 }
-// getComputerChoice()
+// getcomputerValue()
 
 // step 3 --- get human choice
 
@@ -33,3 +33,51 @@ function getHumanChoice() {
 // step 4 --- create score variables
 let humanScore = 0;
 let computerScore = 0;
+
+// step 5 --- Create a round of the game
+
+function playGround(humanChoice,computerChoice) {
+
+const draw = "It's a tie!";
+const scissorsWin = "Scissors beats Paper!";
+const paperWin = "Paper beats Rock!"; 
+const rockWin = "Rock beats Scissors!"
+const win = "You win";
+const lose = "You lose";
+
+//try humanChoice.toLowerCase()
+
+if (humanChoice == computerChoice) {
+     console.log(draw);
+}
+else if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log(lose + ", " + paperWin);
+   return ++computerScore
+}
+else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log(win + ", " + rockWin);
+   return ++humanScore
+}
+else if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log(win + ", " + paperWin);
+   return ++humanScore
+}
+else if (humanChoice == "paper" && computerChoice == "scissors") {
+    console.log(lose + ", " + scissorsWin);
+   return ++computerScore
+}
+else if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log(win + ", " + scissorsWin);
+   return ++humanScore
+}
+else if (humanChoice == "scissors" && computerChoice == "rock") {
+    console.log(lose + ", " + rockWin);
+   return ++computerScore
+}
+}
+
+const humanSelection = getComputerChoice();
+const computerSelection = getComputerChoice();
+
+ playGround(humanSelection, computerSelection);
+
