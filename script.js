a08bc0a67bde7ce30423bcd488042bf662bcd801
0la-lekan <abdulmuizolawoyin@gmail.com>
2025-06-19 02:24:27 +0100

    Get computer choice Create function to get computer choice using Math.random() and conditional statements

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-// Step 1
+// Step 1 --- testing console
 console.log("Hello World");
+
+// step 2 --- generate computer choice
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random()*100);
+    let computerChoice;
+    // console.log(randomNum);
+
+    if (randomNum >= 0 && randomNum < 31) {
+         computerChoice = "Rock";
+         return computerChoice;
+    }
+    else if (randomNum >= 31 && randomNum < 61) {
+        computerChoice = "Paper";
+        return computerChoice;
+    }
+    else {
+        computerChoice = "Scissors";
+        return computerChoice;
+    }
+}
+console.log(getComputerChoice())
